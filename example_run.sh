19d460bf5856fdac9676440cb9dbe580078ff209
Felix <onceltuca@gmail.com>
2020-02-12 10:46:02 +0100

    Moved everything to benchmark folder. Doing al simulations one by one in example)run_recatores.R

--- example_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-Rscript scripts/sample_dags.R --filename dags.rds --nodes 20 --parents 2 --samples 4 --seed 1
-Rscript scripts/sample_bayesian_network_for_dag.R --input_filename dags.rds --filename bns.rds --seed 1
-Rscript scripts/sample_data.R --filename data2n.rds --filename_bn bns.rds --samples 40 --seed 1
-Rscript scripts/sample_data.R --filename data10n.rds --filename_bn bns.rds --samples 200 --seed 1
-Rscript scripts/run_simulations.R --filename_dags dags.rds --filename_datas data2n.rds data10n.rds --timesvec 4 5 6
-Rscript scripts/plot_results.R
--- lib/code_for_binary_simulations/binary_DF.R
+++
@@ -1,7 +0,0 @@
-## This seems to an intermediate step in between simulation an plotting.
-library(stringr)
-source("lib/code_for_binary_simulations/summarySE.R")
-source("lib/code_for_binary_simulations/df_fns.R")
-
-
-
