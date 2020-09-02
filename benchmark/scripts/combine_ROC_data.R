d9e0bae995c332fb93e5d619fe5046c217cfce03
Felix Rios <felixleopoldo.rios@unibas.ch>
2020-09-02 12:00:50 +0200

    added tabu

--- benchmark/scripts/combine_ROC_data.R
+++ benchmark/scripts/combine_ROC_data.R
@@ -26,8 +26,6 @@ for(alg_name in names(config$algorithms)) {
     }
 }
 active_algorithms <- unique(active_algorithms)
-print(active_algorithms)
-#print(config$plotting$algorithms)
 
 if("ordermcmc" %in% active_algorithms) {
     ROCdf_order_mcmc <- read.csv("simresults/ordermcmc.csv")
@@ -58,35 +56,6 @@ if("ordermcmc" %in% active_algorithms) {
     toplot <- dplyr::bind_rows(toplot, sumROC_order_mcmc)
 }
 
-# if("ordermcmc_map" %in% config$plotting$algorithms) {
-#     ROCdf_order_mcmc <- read.csv("simresults/ordermcmc_map.csv")
-#     sumROC_order_mcmc = ROCdf_order_mcmc %>%
-#                         #filter(dim %in% dims) %>%
-#                         #filter(sample_size %in% sample_sizes) %>%
-#                         #filter(avparents %in% dag_avparents) %>%
-#                         filter(replicate %in% replicates) %>%
-#                         # filter(is.na(itsearch_plus1it) | (itsearch_plus1it %in% config$itsearch_sample$optional$plus1it)) %>%
-#                         # group_by(legend, !!as.symbol("threshold"), sample_size, dim, avparents) %>% 
-#                         group_by(legend, adjmat, bn, data, !!as.symbol("threshold")) %>% 
-#                         summarise(SHD_mean = mean(SHD),
-#                                   TPR_mean = mean(TPR), 
-#                                   TPR_median = median(TPR), 
-#                                   FPRn_median = median(FPRn), 
-#                                   TPR_q1 = quantile(TPR, probs = c(0.05)), 
-#                                   TPR_q3 = quantile(TPR, probs = c(0.95)),
-#                                   time_mean = mean(time),
-#                                   logscore_mean = mean(logscore),
-#                                   N = n()) #%>%
-#                         #filter(N %in% length(replicates))
-
-#     #labels <- apply(sumROC_order_mcmc, 1, function(row) {
-#     #    paste("p=",row["dim"], ", n=",row["sample_size"], ", avparents=", row["avparents"], ", N=",row["N"] , sep="")
-#     #})
-#     #sumROC_order_mcmc["labels"] <- labels
-#     sumROC_order_mcmc["labels"] <- NA
-#     toplot <- dplyr::bind_rows(toplot, sumROC_order_mcmc)
-# }
-
 if("itsearch" %in% active_algorithms) {
     ROCdf_itsearch <-read.csv("simresults/itsearch.csv")
     sum_roc_itsearch <- ROCdf_itsearch %>% 
@@ -116,34 +85,6 @@ if("itsearch" %in% active_algorithms) {
     toplot <- dplyr::bind_rows(toplot, sum_roc_itsearch)
 }
 
-# if("itsearch_sample" %in% config$plotting$algorithms) {
-#     ROCdf_itsearch <-read.csv("simresults/itsearch.csv")
-#     sum_roc_itsearch <- ROCdf_itsearch %>% 
-#                     #filter(!!as.symbol("dim") %in% dims) %>%
-#                     #filter(sample_size %in% sample_sizes) %>%
-#                     #filter(avparents %in% dag_avparents) %>%
-#                     #filter(is.na(plus1it) | (plus1it %in% config$algorithms$itsearch_sample$optional$plus1it)) %>%
-#                      group_by(legend, adjmat, bn, data) %>% 
-#                      summarise( SHD_mean = mean(SHD),
-#                                TPR_mean = mean(TPR), 
-#                               TPR_median = median(TPR), 
-#                               FPRn_median = median(FPRn), 
-#                               TPR_q1 = quantile(TPR, probs = c(0.05)), 
-#                               TPR_q3 = quantile(TPR, probs = c(0.95)),
-#                               logscore_mean =  mean(logscore),
-#                                time_mean = mean(time),
-#                              #  it_mean = mean(it),
-#                               N = n()) #%>%
-#                         #filter(N %in% length(replicates))0
-
-#     #labels <- apply(sum_roc_itsearch, 1, function(row) {
-#     #    paste("p=",row["dim"],", n=",row["sample_size"], ", avparents=", row["avparents"], ", N=",row["N"] , sep="")
-#     #})
-
-#     #sum_roc_itsearch["labels"] <- labels 
-#     sum_roc_itsearch["labels"] <- NA
-#     toplot <- dplyr::bind_rows(toplot, sum_roc_itsearch)
-# }
 
 if("pcalg" %in% config$plotting$algorithms) {
     roc_pcalg <-read.csv("simresults/pcalg.csv")
@@ -155,6 +96,35 @@ if("pcalg" %in% config$plotting$algorithms) {
                     filter(replicate %in% replicates) %>%
                      #group_by(legend, !!as.symbol("alpha"), sample_size, dim, avparents) %>% 
                      group_by(legend, adjmat, bn, data, !!as.symbol("alpha")) %>% 
+                     summarise(    
+                           SHD_mean = mean(SHD),
+                              TPR_mean = mean(TPR), 
+                              TPR_median = median(TPR), 
+                              FPRn_median = median(FPRn), 
+                              TPR_q1 = quantile(TPR, probs = c(0.05)), 
+                              TPR_q3 = quantile(TPR, probs = c(0.95)),
+                              time_mean = mean(time),
+                              N = n())# %>% ungroup() %>%
+    #                 filter(N %in% replicates)
+    #labels <- apply(sum_roc_pcalg, 1, function(row) {
+    #    paste("p=",row["dim"], ", n=",row["sample_size"], ", avparents=", row["avparents"], ", N=",row["N"] , sep="")
+    #})
+
+    #sum_roc_pcalg["labels"] <- labels 
+    sum_roc_pcalg["labels"] <- NA   
+    toplot <- dplyr::bind_rows(toplot, sum_roc_pcalg)
+}
+
+if("tabu" %in% config$plotting$algorithms) {
+    roc_pcalg <-read.csv("simresults/tabu.csv")
+
+    sum_roc_pcalg <- roc_pcalg %>% 
+                     #filter(dim %in% dims) %>%
+                     #filter(sample_size %in% sample_sizes) %>%         
+                     #filter(avparents %in% dag_avparents) %>%
+                    filter(replicate %in% replicates) %>%
+                     #group_by(legend, !!as.symbol("alpha"), sample_size, dim, avparents) %>% 
+                     group_by(legend, adjmat, bn, data) %>% 
                      summarise(    
                            SHD_mean = mean(SHD),
                               TPR_mean = mean(TPR), 
--- benchmark/scripts/join_csv_files.R
+++ benchmark/scripts/join_csv_files.R
@@ -7,35 +7,21 @@ p <- add_argument(p, "--algorithm", help = "Algorithm name")
 p <- add_argument(p, "--filename", help = "Output filename")
 argv <- parse_args(p)
 directory <- "simresults"
-#filenames <- list.files(pattern = "csv", path = file.path(directory,"result", "algorithm=", argv$algorithm), recursive = TRUE)
-#print(filenames)
 df <- data.frame()
 
-#for (filename in filenames) {
-#  tmpdf <- read.csv(file.path(directory,"result", "algorithm=", argv$algorithm, filename)) 
-#  #tmpdf["plus1it"] <- na_if(tmpdf["plus1it"], "None")
-#  #tmpdf["posterior"] <- na_if(tmpdf["posterior"], "None")
-#  tmpdf <- na_if(tmpdf, "None")
-#  df <- dplyr::bind_rows(df, tmpdf)
-#}
-
 for (filename in argv$files) {
-  #print(filename)
   tmpdf <- read.csv(filename)
-  #print(tmpdf)
+
   tmpdf <- na_if(tmpdf, "None")
-  #tmpdf["plus1it"] <- na_if(tmpdf["plus1it"], "None")
-  #tmpdf["posterior"] <- na_if(tmpdf["posterior"], "None")
-  
+
   if("plus1it" %in% colnames(tmpdf)) {
       tmpdf["plus1it"] <- as.numeric(tmpdf["plus1it"])
   }
   if("posterior" %in% colnames(tmpdf)) {
       tmpdf["posterior"] <- as.numeric(tmpdf["posterior"])
   }
-  #print(str(tmpdf))
-  #print("Ny rad")
+
   df <- dplyr::bind_rows(df, tmpdf)
 }
-print(df)
+
 write.csv(df, file=argv$filename, row.names=FALSE)
