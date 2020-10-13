79c47b707f5a0e4fbb493f1627734c7f386a7c0a
Felix Rios <felixleopoldo.rios@unibas.ch>
2020-10-13 10:40:52 +0200

    Generate dags parameters, and data using notears.

--- benchmark/scripts/notears/simulate_from_dag_lg.py
+++ benchmark/scripts/notears/simulate_from_dag_lg.py
@@ -1,22 +1,15 @@
 import argparse
 import numpy as np
 import notears
-#import pandas as pd
+import pandas as pd
 
 def main(n_samples, mean, variance, filename, weighted_adjmat_filename, seed):
-
-    #adjmat = np.genfromtxt(weighted_adjmat_filename, dtype=float, skip_header=1)
-    adjmat = np.loadtxt(weighted_adjmat_filename, delimiter=",",skiprows=1, dtype=float)
-    print(adjmat)
-    data = np.array(notears.utils.simulate_from_dag_lg(  adjmat, 
-                                                n_samples, 
-                                                mean=mean, 
-                                                variance=variance))
-    print(data)
-    header = np.array([list(range(data.shape[1]))])
-    tosave = np.append(header,data, axis=0)
-
-    np.savetxt(filename, tosave, delimiter=" ")
+    adjmat_df = pd.read_csv(weighted_adjmat_filename)
+    data = np.array(notears.utils.simulate_from_dag_lg(adjmat_df.values, 
+                                                        n_samples, 
+                                                        mean=mean, 
+                                                        variance=variance))
+    pd.DataFrame(data).to_csv(filename, header=adjmat_df.columns, sep=" ", index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Sample DAG.")
