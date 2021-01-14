54b23729ec9c81f66503e8c3f45215910ff2d66a
Felix Rios <felixleopoldo.rios@unibas.ch>
2021-01-14 15:24:22 +0100

    Fixed bug with notears parameters and graph. Added new config sample for Gaussian SEM.

--- workflow/scripts/notears/generate_random_dag.py
+++ workflow/scripts/notears/generate_random_dag.py
@@ -8,7 +8,6 @@ def main(num_nodes, num_edges, filename, seed):
     edge_coefficient_range = [-1, 1]
     np.random.seed(seed)
     true_adj_mat, _ = notears.utils.generate_random_dag(num_nodes, num_edges, edge_coefficient_range=edge_coefficient_range)
-
     true_adj_mat = abs(true_adj_mat) > 0
     header = np.array([list(range(len(true_adj_mat)))])
     tosave = np.append(header,true_adj_mat, axis=0)
