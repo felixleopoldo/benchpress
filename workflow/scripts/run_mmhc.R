c8c75632cf39a208f361ebc0e6f617e2baf69a84
Felix Rios <felixleopoldo.rios@unibas.ch>
2021-03-02 09:32:43 +0100

    Fixed bug in MMHC.

--- workflow/scripts/run_mmhc.R
+++ workflow/scripts/run_mmhc.R
@@ -11,6 +11,7 @@ seed <- as.integer(snakemake@wildcards[["replicate"]])
 data <- read.csv(filename_data, sep=",")
 
 header <- names(data)
+# Implicitly checking it discrete data.
 if(snakemake@wildcards[["test"]] %in% c("mi", "mi-adf", "mc-mi", "smc-mi", 
                                          "sp-mi", "mi-sh", "x2", "x2-adf", "sp-x2",
                                          "jt", "mc-jt", "smc-jt")){
@@ -20,17 +21,20 @@ if(snakemake@wildcards[["test"]] %in% c("mi", "mi-adf", "mc-mi", "smc-mi",
 set.seed(seed)
 
 start <- proc.time()[1]
-mmoutput <- mmhc(data, restrict.args = list(    
-    as.numeric(snakemake@wildcards[["iss"]),
-    as.numeric(snakemake@wildcards[["iss.mu"]),
-    as.numeric(snakemake@wildcards[["l"]),
-    as.numeric(snakemake@wildcards[["k"]),
-    snakemake@wildcards[["prior"]),
-    as.numeric(snakemake@wildcards[["beta"]),
-    score = snakemake@wildcards[["score"]]
-    alpha = as.numeric(snakemake@wildcards[["alpha"]]),
-    test=snakemake@wildcards[["test"]])
-
+mmoutput <- mmhc(data, 
+                restrict.args = list(    
+                    alpha = ifelse(snakemake@wildcards[["alpha"]]=="None", NULL, as.numeric(snakemake@wildcards[["alpha"]])),
+                    test = snakemake@wildcards[["test"]]),
+                maximize.args =  list(
+                    iss = ifelse(snakemake@wildcards[["iss"]]=="None", 1, as.numeric(snakemake@wildcards[["iss"]])),
+                    iss.mu = ifelse(snakemake@wildcards[["issmu"]]=="None", 1, as.numeric(snakemake@wildcards[["issmu"]])),
+                    l = ifelse(snakemake@wildcards[["l"]]=="None", 5, as.numeric(snakemake@wildcards[["l"]])),
+                    k = ifelse(snakemake@wildcards[["k"]]=="None", 1, as.numeric(snakemake@wildcards[["k"]])),                                
+                    prior = snakemake@wildcards[["prior"]],
+                    beta = as.numeric(snakemake@wildcards[["beta"]]),
+                    score = snakemake@wildcards[["score"]]
+                    )
+                )
 totaltime <- proc.time()[1] - start
 
 ## convert to graphneldag
