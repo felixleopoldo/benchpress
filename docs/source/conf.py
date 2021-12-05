4d7183aa12fa812e169a24cac46532ae47d4aa2b
Felix Rios <felix.leopoldo.rios@gmail.com>
2021-12-05 15:22:27 +0100

    update logo in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,15 @@ numfig = True
 #
 html_theme = 'sphinx_rtd_theme'
 
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+html_logo = '_static/benchpress-logo-readthedocs@2x.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
