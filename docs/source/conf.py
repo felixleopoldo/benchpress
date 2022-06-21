a42f43568c052e3dda8bc82d7ac6bdfd28746ac2
Felix Rios <34372003+felixleopoldo@users.noreply.github.com>
2022-06-21 10:19:20 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Benchpress'
-copyright = '2020, Felix Rios.'
+copyright = '2022, Felix Rios.'
 author = 'Felix Rios'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
