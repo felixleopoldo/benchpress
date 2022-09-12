1cc51e5b0b19c9e032edf1a1f19897c29277c24a
Felix Rios <felix.leopoldo.rios@gmail.com>
2022-09-12 17:09:05 +0200

    Updated docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '1.2.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx-jsonschema']
+# extensions = ['sphinx-jsonschema']
 # source_suffix = {
 #     '.rst': 'restructuredtext',
 #     '.txt': 'markdown',
