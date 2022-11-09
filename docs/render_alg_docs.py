c0ec7e0807a3d340b1fba297b7a63eb11724b3fe
Felix Rios <felix.rios@rocafuerte.se>
2022-11-09 11:31:17 +0100

    Tables from info files in docs.

--- docs/render_alg_docs.py
+++ docs/render_alg_docs.py
@@ -4,16 +4,87 @@ import json
 """
 """
 
+def info_to_table(json, p):
+    tab = ".. list-table:: \n\n"#+p.name+"\n\n"
+    #tab += "   * - Title\n"
+    #tab += "     - "+info["title"]+"\n"
+    tab += "   * - Package\n"
+    tab += "     - `"+info["package"]["title"]+" <"+info["package"]["url"]+">`_\n"
+    tab += "   * - Version\n"
+    tab += "     - "+info["version"]+"\n"
+    tab += "   * - Language\n"
+    tab += "     - "+info["language"]+"\n"
+    tab += "   * - Docs\n"
+    tab += "     - `here <"+info["docs_url"]+">`_\n"
+    tab += "   * - Paper\n"
+    for i in range(len(info["papers"])):
+        tab += "     - `"+info["papers"][i]["title"]+" <"+info["papers"][i]["url"]+">`_, "  
+    tab = tab[:-2]
+    tab += "\n"
+    tab += "   * - Graph type\n"
+    for i in range(len(info["graph_types"])):
+        tab += "     - "+info["graph_types"][i] +", "
+    tab = tab[:-2]
+    tab += "\n"
+    tab += "   * - Docker\n"
+    tab += "     - `"+info["docker_image"]+" <"+info["docker_image"]+">`_\n"
+    tab += "   * - Module\n"
+    tab += "     - `"+p.name+" <https://github.com/felixleopoldo/benchpress/tree/master/workflow/rules/structure_learning_algorithms/"+p.name+">`_\n"
+    
+
+    tab += "\n"
+    return tab
+
+def info_to_table2(json, p):
+    
+    tab = "| Title: "
+    tab += p.name +"\n"
+    tab += "| Package: "
+    tab += "`"+info["package"]["title"]+" <"+info["package"]["url"]+">`_ \n"
+    tab += "| Paper: "
+    for i in range(len(info["papers"])):
+        tab += "`"+info["papers"][i]["title"]+" <"+info["papers"][i]["url"]+">`_, "
+    tab += ""
+    tab = tab[:-2]
+    tab += "\n"
+    tab += "| Graph types: "
+    for i in range(len(info["graph_types"])):
+        tab += ""+info["graph_types"][i] +", "
+
+    tab = tab[:-2]
+
+    tab += "\n"
+    tab += "| Version: "
+    tab += ""+info["version"]+"\n"
+    tab += "| Docs: "
+    tab += "`here <"+info["docs_url"]+">`_\n"
+    tab += "| Docker image: "
+    tab += "`"+info["docker_image"]+" <"+info["docker_image"]+">`_ \n"
+    tab += "| Language: "
+    tab += ""+info["language"]+"\n"
+    tab += "| Module directory: "
+    tab += "`"+p.name+" <https://github.com/felixleopoldo/benchpress/tree/master/workflow/rules/structure_learning_algorithms/"+p.name+">`_\n"
+    
+
+    tab += "\n"
+    return tab
+
+
+
 algspath = Path("../workflow/rules/structure_learning_algorithms")
 
 
 
 for p in algspath.iterdir():
-    # print(p)
+    print(p.name)
+    if p.name == "docs.rst":
+        continue
     d = p/"docs.rst"
     j = p/"info.json"
-
+    s = p/"schema.json"
+    
     if not j.is_file():
+    
         info = {
             "title": p.name,
             "docker_image": "docker://",
@@ -35,15 +106,38 @@ for p in algspath.iterdir():
         
         with open(j, "w") as outfile:
             outfile.write(json.dumps(info, indent=4))
-str = ""
 
-for p in algspath.iterdir():
-    #print(p.name)
+
+    with open(s) as json_file:    
+        schema = json.load(json_file)
     
-    str += "\n\n"
-    str += "\n\n"
-    str +="``" + p.name +"`` \n"
-    str +="-"*len(p.name) + "-"*4 + "\n"
+    if "$ref" in schema["items"]:
+        schema["items"] = schema["definitions"][p.name]
+    
+    if "definitions" in schema:
+        schema.pop("definitions")
+
+    # with open(s, "w") as outfile:
+    #     outfile.write(json.dumps(schema, indent=4))
+
+    #print(schema)
+
+if (algspath / "docs.rst").is_file():
+    f = open(algspath / "docs.rst", "r")
+    content = f.read()
+
+
+str = ""
+str += "``"+algspath.name+"``\n"
+str += "="*len(algspath.name) + "="*10
+str += "\n\n"
+str += content
+str += "\n\n"
+for p in sorted(algspath.iterdir()):
+    #print(p.name)
+    if p.name == "docs.rst":
+        continue
+    
     d = p/"docs.rst"
     j = p/"info.json"
     s = p/"schema.json"
@@ -53,58 +147,29 @@ for p in algspath.iterdir():
 
     with open(j) as json_file:
         info = json.load(json_file)
-        #info = json.dumps(info, indent=2)
-        #print(info)
+
     
     schema = None    
     with open(s) as json_file:    
         schema = json.load(json_file)
     
-    if "definitions" in schema:
-        if "examples" in schema["definitions"][p.name]:
-            dump = json.dumps(schema["definitions"][p.name]["examples"], indent=2)
-
-
-    tab = ".. list-table:: infotable\n\n"
-    tab += "    :widths: 15 10 \n"
-    tab += "    :header-rows: 0\n\n"
-    tab += "    *  - Title\n"
-    tab += "       - "+p.name+"\n"
-    tab += "    *  - Package\n"
-    tab += "       - `"+info["package"]["title"]+"<"+info["package"]["url"]+">`_\n"
-
-    tab += "    *  - Paper\n"
-    for i in range(len(info["papers"])):
-        tab += "       - `"+info["papers"][i]["title"]+"<"+info["papers"][i]["url"]+">`_, "
-    tab += "\n"
-    tab = tab[:-2]
-    tab += "\n"
-    tab += "    *  - Graph types\n"
-    for i in range(len(info["graph_types"])):
-        tab += "       - "+info["graph_types"][i] +", "
-
-    tab = tab[:-2]
-
-    tab += "\n"
-    tab += "    *  - Version\n"
-    tab += "       - "+info["version"]+"\n"
-    tab += "    *  - Docs\n"
-    tab += "       - `here<"+info["docs_url"]+">`_\n"
-
-    tab += "    *  - Docker image\n"
-    tab += "       - "+info["docker_image"]+"\n"
-    tab += "    *  - Language\n"
-    tab += "       - "+info["language"]+"\n"
-    tab += "    *  - Module directory\n"
-    tab += "       - `"+p.name+"<https://github.com/felixleopoldo/benchpress/tree/master/workflow/rules/structure_learning_algorithms/"+p.name+">`_\n"
+        if "examples" in schema["items"]:
+            dump = json.dumps(schema["items"]["examples"], indent=2)
+        else: dump = ""
     
-    tab += "\n"
-    tab += "\n"
-    tab += "\n"
+  
+    str += "\n\n"
+    str += "\n\n"
+    str +="``" + p.name +"`` \n"
+    str +="-"*len(p.name) + "-"*4 + "\n"
 
+    
     str += "\n\n"
-    str += tab
+    str += ".. rubric:: "+ info["title"]    
     str += "\n\n"
+    str += info_to_table(info, p)
+    str += "\n\n"
+    str += ".. rubric:: Description"    
     if content != "":    
         str += "\n\n"
         str += content
@@ -115,5 +180,8 @@ for p in algspath.iterdir():
     str += "\n\n"
     str += '    '.join(('\n'+dump.lstrip()).splitlines(True))
     
-
+    
 print(str)
+
+with open("source/available_structure_learning_algorithms.rst", "w") as text_file:
+    text_file.write(str)
