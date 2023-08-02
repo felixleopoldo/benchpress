d85735b86492253c2e49c0a1109c69250cf43ad3
Felix Rios <felix.leopoldo.rios@gmail.com>
2023-08-02 17:08:09 +0200

    Updating config.json to fges new format.

--- workflow/scripts/utils/tetrad_graph_to_adjmat.R
+++ workflow/scripts/utils/tetrad_graph_to_adjmat.R
@@ -1,4 +1,4 @@
-# Converts a graph in TETRAD format into an adjacency matrix.
+# Converts a graph in TETRAD (causal-cmd 1.9.0) format into an adjacency matrix.
 
 library(argparser)
 library(rjson)
@@ -23,10 +23,6 @@ fges_result_to_matrix <- function(graph) {
     node2 <- e$node2$name
     node1_ind <- match(node1, nodes) # TODO: This might not always work..
     node2_ind <- match(node2, nodes)
-
-    #isdirected <- e$endpoint2$ordinal && TRUE
-    #isdirected <- e$endpoint2$ordinal && TRUE
-    #isdirected <- e$node2$nodeType$ordinal && TRUE
 
     isdirected <- (e$endpoint1 == "TAIL") && (e$endpoint2 == "ARROW")
 
