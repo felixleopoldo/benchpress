0ee8df6fc6b1400b480e9d4ca510f4d6e73ae7b1
Felix Rios <felixleopoldo.rios@unibas.ch>
2023-09-21 13:04:35 +0200

    Minor fixes.

--- workflow/rules/evaluation/benchmarks/plot_ROC.R
+++ workflow/rules/evaluation/benchmarks/plot_ROC.R
@@ -656,7 +656,7 @@ elapsed_time <- function(){
               )
             }
           } +
-          facet_wrap(. ~ adjmat + parameters + data + n_seeds, ncol = 2, scales = "free_x") +
+          facet_wrap(. ~ adjmat + parameters + data, ncol = 2, scales = "free_x") +
           {
             if (!is.null(xlim)) {
               xlim(xlim[1], xlim[2])
@@ -715,7 +715,7 @@ elapsed_log_time <- function(){
               )
             }
           } +
-          facet_wrap(. ~ adjmat + parameters + data + n_seeds, ncol = 2, scales = "free_x") +
+          facet_wrap(. ~ adjmat + parameters + data, ncol = 2, scales = "free_x") +
           {
             if (!is.null(xlim)) {
               xlim(xlim[1], xlim[2])
