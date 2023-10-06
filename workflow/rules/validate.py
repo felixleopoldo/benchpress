bcd2732b442d8eb4a73a1e8ea67f1bb8d3aad4ae
Felix Rios <felix.leopoldo.rios@gmail.com>
2023-10-06 13:02:25 +0200

    ON the input graph feature.

--- workflow/rules/validate.py
+++ workflow/rules/validate.py
@@ -4,26 +4,30 @@
 # Check that algorithm exists
 
 # Available algorithm ids
+from typing import Optional, List, Union, Tuple
 available_conf_ids = []
 for alg, alg_conf_avail in config["resources"]["structure_learning_algorithms"].items():
     for alg_conf in alg_conf_avail:
-        available_conf_ids.append(alg_conf["id"])   
+        available_conf_ids.append(alg_conf["id"])
 
 # Check that all ids in the benchmarks section actually exist.
 for benchmarksitem in config["benchmark_setup"]["evaluation"]["benchmarks"]["ids"]:
     if benchmarksitem not in available_conf_ids:
-        raise Exception(benchmarksitem + " not available.\nThe available id's are:\n{ids}".format(ids=sorted(available_conf_ids)))
+        raise Exception(
+            benchmarksitem + " not available.\nThe available id's are:\n{ids}".format(ids=sorted(available_conf_ids)))
 
 # Check that all ids in the graph_plots actually exist.
 for benchmarksitem in config["benchmark_setup"]["evaluation"]["graph_plots"]:
     if benchmarksitem not in available_conf_ids:
-        raise Exception(benchmarksitem + " not available.\nThe available id's are:\n{ids}".format(ids=sorted(available_conf_ids)))
+        raise Exception(
+            benchmarksitem + " not available.\nThe available id's are:\n{ids}".format(ids=sorted(available_conf_ids)))
 
 # Check that the startspace for order mcmc exist.
 # for alg_conf in config["resources"]["structure_learning_algorithms"]["bidag_order_mcmc"]:
-#     if alg_conf["startspace_algorithm"] not in set(available_conf_ids + [None]) - {c["id"] for c in config["resources"]["structure_learning_algorithms"]["bidag_order_mcmc"]}:
-#         raise Exception(alg_conf["startspace_algorithm"] + " not available startspace for order_mcmc.\n"\
+#     if alg_conf["input_algorithm_id"] not in set(available_conf_ids + [None]) - {c["id"] for c in config["resources"]["structure_learning_algorithms"]["bidag_order_mcmc"]}:
+#         raise Exception(alg_conf["input_algorithm_id"] + " not available startspace for order_mcmc.\n"\
 #                         "The available are:\n"+str(sorted(list(set(available_conf_ids) - {c["id"] for c in config["resources"]["structure_learning_algorithms"]["bidag_order_mcmc"]}))))
+
 
 def validate_data_setup(config, dict):
     # Check that adjmat exists
@@ -40,7 +44,7 @@ def validate_data_setup(config, dict):
                             "The available graph id´s are:\n" + str(sorted(available_conf_ids)))
 
         if not dict["graph_id"] in available_conf_ids:
-            raise Exception(dict["graph_id"] + 
+            raise Exception(dict["graph_id"] +
                             " is not an available graph id.\n"
                             "The available graph id´s are:\n" + str(sorted(available_conf_ids)))
 
@@ -51,10 +55,9 @@ def validate_data_setup(config, dict):
                             "The available graph id´s are:\n" + str(sorted(available_conf_ids)))
 
         if not dict["graph_id"] in available_conf_ids:
-            raise Exception(dict["graph_id"] + 
+            raise Exception(dict["graph_id"] +
                             " is not an available graph id.\n"
                             "The available graph id´s are:\n" + str(sorted(available_conf_ids)))
-
 
     available_data_files = os.listdir("resources/data/mydatasets")
 
@@ -66,55 +69,45 @@ def validate_data_setup(config, dict):
     available_conf_ids += available_data_files
 
     if dict["data_id"] not in available_conf_ids:
-        raise Exception(str(dict["data_id"]) + 
+        raise Exception(str(dict["data_id"]) +
                         " is not an available data id.\n"
                         "The available data id´s are:\n" + str(sorted(available_conf_ids)))
     # Check that graph, parameters, and data are properly combined.
-
 
     # Check that parameters exists
     available_conf_ids = []
     for alg, alg_conf_avail in config["resources"]["parameters"].items():
         for alg_conf in alg_conf_avail:
             available_conf_ids.append(alg_conf["id"])
-    available_conf_ids += os.listdir("resources/parameters/myparams/bn.fit_networks")
-    available_conf_ids += os.listdir("resources/parameters/myparams/sem_params")
+    available_conf_ids += os.listdir(
+        "resources/parameters/myparams/bn.fit_networks")
+    available_conf_ids += os.listdir(
+        "resources/parameters/myparams/sem_params")
 
     if dict["data_id"] not in available_data_files and dict["parameters_id"] not in available_conf_ids:
-        raise Exception(str(dict["parameters_id"])+ 
+        raise Exception(str(dict["parameters_id"]) +
                         " is not an available parameter id.\n"
                         "The available paremeter id´s are:\n" + str(sorted(available_conf_ids)))
 
-
-
-   
+# validate the data_setup
 for data_setup in config["benchmark_setup"]["data"]:
     validate_data_setup(config, data_setup)
 
 
-    
-from typing import Optional, List, Union, Tuple
-
-""" set of functions to nest algorithms """
 def valid_path(run_id: Optional[str]) -> bool:
     """ Returns the path to the estimated graph of each id"""
-    
+
     if not run_id:
         return False
-    
+
     alg, run_config = idtoalg(run_id)
     if not alg:
         return False
-    
+
     if not is_single_output_run(run_config):
         return False
-    
-    return True
-    # if alg in mcmc_modules:
-    #     return expand(pattern_strings[alg]+"/"+pattern_strings["mcmc_est"], **run_config)
-    # else:
-    #     return expand(pattern_strings[alg], **run_config)
 
+    return True
 
 def idtoalg(run_id: str) -> Optional[Tuple[str, dict]]:
     """ Returns the algorithm name that the id belongs to, otherwise None """
@@ -127,15 +120,14 @@ def idtoalg(run_id: str) -> Optional[Tuple[str, dict]]:
 
 def is_single_output_run(run_config: dict) -> bool:
     """ checks if the run is a single output run, i.e., no parameters is with a list lager > 1"""
-    
+
     for key, item in run_config.items():
         if isinstance(item, list):
-            if len(item) > 1: 
+            if len(item) > 1:
                 return False
-    
+
     return True
-    
- 
+
 
 def validate_algorithms():
     """ Random validation tests for config"""
@@ -146,13 +138,14 @@ def validate_algorithms():
                 if isinstance(run['input_algorithm_id'], list):
                     for input_graph in run['input_algorithm_id']:
                         path = valid_path(input_graph)
-                        if not path: 
-                            raise Exception(f"In algorithm {key}, 'input_algorithm_id' {input_graph} is not available in the config file, or the associated config has parameters with multiple values!")
+                        if not path:
+                            raise Exception(
+                                f"In algorithm {key}, 'input_algorithm_id' {input_graph} is not available in the config file, or the associated config has parameters with multiple values!")
                 else:
                     path = valid_path(run['input_algorithm_id'])
-                    if not path: 
-                        raise Exception(f"In algorithm {key}, 'input_algorithm_id' {run['input_algorithm_id']} is not available in the config file, or the associated config has parameters with multiple values!")
-    
+                    if not path:
+                        raise Exception(
+                            f"In algorithm {key}, 'input_algorithm_id' {run['input_algorithm_id']} is not available in the config file, or the associated config has parameters with multiple values!")
 
 
 validate_algorithms()
