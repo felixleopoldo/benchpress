37b4fe32b4174d64d76204d7074bcda05d6a1f65
Alex Markham <markham@kth.se>
2023-11-06 14:13:56 -0600

    Fixing the input graph feature.

--- workflow/rules/filename_patterns.py
+++ workflow/rules/filename_patterns.py
@@ -1,69 +1,79 @@
 # A number of code snippets, just to reduce duplication.
 # The function names are sometimes a bit misleading.
 
-####  MCMC stuff
+# MCMC stuff
 def adjmat_estimate_path_mcmc(algorithm):
     ret = "{output_dir}/adjmat_estimate/"\
-                        "adjmat=/{adjmat}/"\
-                        "parameters=/{bn}/"\
-                        "data=/{data}/"\
-                        "algorithm=/" + pattern_strings[algorithm] + "/" + pattern_strings["mcmc_est"] + "/" \
-                        "seed={replicate}/" \
-                        "adjmat.csv"
+        "adjmat=/{adjmat}/"\
+        "parameters=/{bn}/"\
+        "data=/{data}/"\
+        "algorithm=/" + pattern_strings[algorithm] + "/" + pattern_strings["mcmc_est"] + "/" \
+        "seed={replicate}/" \
+        "adjmat.csv"
     return ret
+
 
 def alg_output_seqgraph_path_fine_match(algorithm):
     return "{output_dir}/adjvecs/"\
-                "adjmat=/{adjmat}/"\
-                "parameters=/{bn}/"\
-                "data=/{data}/"\
-                "algorithm=/" + pattern_strings[algorithm] + "/"  + \
-                "seed={replicate}/" \
-                "adjvecs.csv"
+        "adjmat=/{adjmat}/"\
+        "parameters=/{bn}/"\
+        "data=/{data}/"\
+        "algorithm=/" + pattern_strings[algorithm] + "/" + \
+        "seed={replicate}/" \
+        "adjvecs.csv"
+
 
 def alg_output_seqgraph_path(algorithm):
     return "{output_dir}/adjvecs/{data}/"\
-               "algorithm=/" + pattern_strings[algorithm] + "/"  + \
-               "seed={replicate}/" \
-               "adjvecs_tobecompressed.csv"
+        "algorithm=/" + pattern_strings[algorithm] + "/" + \
+        "seed={replicate}/" \
+        "adjvecs_tobecompressed.csv"
+
 
 def alg_output_seqgraph_path_nocomp(algorithm):
     return "{output_dir}/adjvecs/{data}/"\
-               "algorithm=/" + pattern_strings[algorithm] + "/"  + \
-               "seed={replicate}/" \
-               "adjvecs.csv"
+        "algorithm=/" + pattern_strings[algorithm] + "/" + \
+        "seed={replicate}/" \
+        "adjvecs.csv"
 
-### Standard algorithms
+# Standard algorithms
+
+
 def alg_output_adjmat_path(algorithm):
     return "{output_dir}/adjmat_estimate/{data}/"\
-                "algorithm=/" + pattern_strings[algorithm] + "/" +\
-                "seed={replicate}/" \
-                "adjmat.csv"
+        "algorithm=/" + pattern_strings[algorithm] + "/" +\
+        "seed={replicate}/" \
+        "adjmat.csv"
+
 
 def alg_output_time_path(algorithm):
     return "{output_dir}/time/{data}/"\
-                "algorithm=/" + pattern_strings[algorithm] + "/" +\
-                "seed={replicate}/" \
-                "time.txt"
+        "algorithm=/" + pattern_strings[algorithm] + "/" +\
+        "seed={replicate}/" \
+        "time.txt"
 
 # This is code repetition, yes...
+
+
 def alg_output_ntests_path(algorithm):
     return "{output_dir}/ntests/{data}/"\
-                "algorithm=/" + pattern_strings[algorithm] + "/" +\
-                "seed={replicate}/" \
-                "ntests.txt"
+        "algorithm=/" + pattern_strings[algorithm] + "/" +\
+        "seed={replicate}/" \
+        "ntests.txt"
+
 
 def alg_input_data():
     return "{output_dir}/data/{data}/seed={replicate}.csv"
 
+
 def time_path(algorithm):
     ret = "{output_dir}/time/"\
-                    "adjmat=/{adjmat}/"\
-                    "parameters=/{bn}/"\
-                    "data=/{data}/"\
-                    "algorithm=/" + pattern_strings[algorithm] + "/" + \
-                    "seed={replicate}/" \
-                    "time.txt"
+        "adjmat=/{adjmat}/"\
+        "parameters=/{bn}/"\
+        "data=/{data}/"\
+        "algorithm=/" + pattern_strings[algorithm] + "/" + \
+        "seed={replicate}/" \
+        "time.txt"
     return ret
 
 
@@ -73,10 +83,3 @@ def data_path():
 
 def adjmat_true_path():
     return "{output_dir}/adjmat/{adjmat}.csv",
-
-
-def path_to_input_algorithm_graph(alg_id):
-    return "{output_dir}/adjmat_estimate/{data}/"\
-        "algorithm=/" + json_string[alg_id][0] + "/" +\
-        "seed={replicate}/" \
-        "adjmat.csv"
