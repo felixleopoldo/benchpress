cd188fa9bcd704d9071968a5433e14da5ec83055
Felix Rios <34372003+felixleopoldo@users.noreply.github.com>
2024-02-16 09:24:38 +0100

    Improving graph_true_plots (#110)
    * In graph_true_stats module: Plotting clique sized for undirected graphs.

--- workflow/rules/evaluation/graph_true_stats/graph_stats.R
+++ workflow/rules/evaluation/graph_true_stats/graph_stats.R
@@ -1,30 +1,51 @@
 library(pcalg)
-adjmat <- read.csv(snakemake@input[["matrix_filename"]])
+library(igraph)
 
-graphnel <- as(t(adjmat), "graphNEL") ## convert to graph
+adjmat <- read.csv(snakemake@input[["matrix_filename"]], check.names = FALSE)
 
 n_edges <- sum(adjmat)
-if (isSymmetric(as.matrix(adjmat))) {
-  n_edges <- n_edges / 2
+if (isSymmetric(unname(as.matrix(adjmat)))) {
+    print("Graph is symmetric")
+    n_edges <- n_edges / 2
 }
 
 sp <- strsplit(snakemake@wildcards[["adjmat_string"]], split = "seed=")
 
 seed <- "None"
 if (length(sp[[1]]) == 2) {
-  seed <- as.integer(sp[[1]][2])
+    seed <- as.integer(sp[[1]][2])
 }
 
-maxin <- max(colSums(adjmat))
+maxin <- max(colSums(adjmat)) # count in-degrees
+max_clique_size <- NA
+avg_clique_size <- NA
+min_clique_size <- NA
+
+# Find the maximal cliques for undirected graphs
+if (isSymmetric(unname(as.matrix(adjmat)))) {
+    my_igraph <- graph_from_adjacency_matrix(
+        as.matrix(adjmat),
+        mode = "undirected",
+        diag = FALSE
+    )
+
+    cliques <- max_cliques(my_igraph)
+    max_clique_size <- max(sapply(cliques, length))
+    avg_clique_size <- mean(sapply(cliques, length))
+    min_clique_size <- min(sapply(cliques, length))
+}
 
 df <- data.frame(
-  "adjmat" = snakemake@wildcards[["adjmat_string"]],
-  "n_inedges" = n_edges,
-  "n_nodes" = ncol(adjmat),
-  "max_indegree" = maxin,
-  "seed" = seed
+    "adjmat" = snakemake@wildcards[["adjmat_string"]],
+    "n_inedges" = n_edges,
+    "n_nodes" = ncol(adjmat),
+    "max_indegree" = maxin,
+    "min_clique_size" = min_clique_size,
+    "max_clique_size" = max_clique_size,
+    "avg_clique_size" = avg_clique_size,
+    "seed" = seed
 )
 
 write.csv(df, snakemake@output[["stats_filename"]],
-  row.names = FALSE, quote = FALSE
+    row.names = FALSE, quote = FALSE
 )
