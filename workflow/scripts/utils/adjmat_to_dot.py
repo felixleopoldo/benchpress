80ed4d8b4e309d559916ada55071765588984c3b
Felix Rios <felix.leopoldo.rios@gmail.com>
2024-05-17 09:03:16 +0200

    Support for plotting graphs with both directed and undirected edges.

--- workflow/scripts/utils/adjmat_to_dot.py
+++ workflow/scripts/utils/adjmat_to_dot.py
@@ -17,9 +17,26 @@ mapping = {i: val for i, val in enumerate(list(df))}
 graph = None
 if (np.transpose(adjmat) == adjmat).all():
     graph = nx.from_numpy_array(adjmat, create_using=nx.Graph())
+    graph = nx.relabel_nodes(graph, mapping)
+    write_dot(graph, dot_filename)
 else:
     graph = nx.from_numpy_array(adjmat, create_using=nx.DiGraph())
+    graph = nx.relabel_nodes(graph, mapping)
 
-graph = nx.relabel_nodes(graph, mapping)
+    # convert to pygrapviz graph and set some edge properties, dir=none for undirected edges
+    agraph = nx.nx_agraph.to_agraph(graph)
+    # iterate over the undirected edges and set the dir attribute to none
+    for i in range(adjmat.shape[0]):
+        for j in range(adjmat.shape[1]):
+            if (adjmat[i, j] == 1) and (adjmat[j, i] == 1):
+                agraph.remove_edge(mapping[j], mapping[i])
+                # add the same edge but without direction
+                agraph.add_edge(mapping[i], mapping[j], dir="none")
+                #agraph.get_edge(mapping[i], mapping[j]).attr['dir' ] = 'none'
+                # remove the other direction
 
-write_dot(graph, dot_filename)
+
+    agraph.draw(dot_filename, prog= 'dot')
+
+
+
